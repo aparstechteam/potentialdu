0a4da2d95ead91928da3bd5f2e5036f9415ec41f
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-08-29 22:46:54 +0600

    updated

---
+++ assets/Home.abf33fe6.js
@@ -0,0 +1,13 @@
+import{r as l,o as t,c as s,a as e,b as a,w as c,d as o}from"./index.06ad8f59.js";const r={class:"text-gray-900"},_={class:"container py-10 mx-auto text-center min-h-screen grid place-items-center"},d={class:"max-w-lg rounded-2xl mx-auto p-3 space-y-5"},h=e("h1",{class:"text-2xl"},"Potential du'ian hunt",-1),u=e("h3",null,"22 Batch",-1),m={key:0,class:"px-3"},p=e("br",null,null,-1),x=e("h2",{class:"text-red-500 font-semibold text-lg"}," Marks Distribution ",-1),f=e("pre",null,`
+Physics 15
+Chemistry 15
+Math 15
+Biology 15
+
+Per question mark 1
+Negative mark 0.5
+
+Total Question: 60
+Total Marks: 60
+Exam Duration: 45 Minutes
+                    `,-1),g={class:"flex justify-center pb-3 gap-3"},b=o("Go to Exam"),v={key:1,class:"space-y-4"},k=e("p",{class:"text-center text-xl"},[o("\u0985\u0982\u09B6\u0997\u09CD\u09B0\u09B9\u09A3 \u0995\u09B0\u09BE\u09B0 \u09B8\u09AE\u09DF \u09B6\u09C7\u09B7\u0964 "),e("br"),o(" \u09B0\u09C7\u099C\u09BE\u09B2\u09CD\u099F \u099C\u09BE\u09A8\u09A4\u09C7 \u09A8\u09BF\u099A\u09C7\u09B0 \u09AC\u09BE\u099F\u09A8\u09C7 \u0995\u09CD\u09B2\u09BF\u0995 \u0995\u09B0\u09CB")],-1),y=e("div",{class:"flex items-center justify-center gap-2 mt-3 md:gap-4 flex-wrap"},[e("a",{href:"https://drive.google.com/file/d/1QFIEoS7fdzWfxNslmwcNweeTMHqGP4BO/view",target:"_blank",class:"btn btn-success"},"Batch 22 Result"),e("a",{href:"https://drive.google.com/file/d/1CM_9W6yu19G17P_Ja4fXnFYi7hQwz4cG/view",target:"_blank",class:"btn btn-success"},"Batch 23 Result")],-1),w=[k,y],T={__name:"Home",setup(B){const i=new Date("1 September 2022").getTime()<Date.now();return JSON.parse(localStorage.getItem("potentialdur")),(N,M)=>{const n=l("router-link");return t(),s("div",r,[e("div",_,[e("div",d,[a(n,{to:"/",as:"div",class:"text-center uppercase"},{default:c(()=>[h,u]),_:1}),i?(t(),s("div",v,w)):(t(),s("div",m,[p,x,f,e("div",g,[a(n,{to:"/exam",class:"btn"},{default:c(()=>[b]),_:1})])]))])])])}}};export{T as default};
